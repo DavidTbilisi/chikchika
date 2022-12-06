942c2963ae26a6d6f1344ffd01015a8d8756957b
DavidCh <DavidChincharashvili@gmail.com>
2022-12-06 15:07:13 +0400

    fix: add token generator to gui

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('tokens.create', ['token_name'=>'temp'])" :active="request()->routeIs('tokens.create')">
+                        {{ __('Create Token') }}
+                    </x-nav-link>
                 </div>
             </div>
 
